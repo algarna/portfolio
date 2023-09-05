613a66e83b33e2ec45df391a66a515e1d3fa3507
Alberto Garcia <algarna85@gmail.com>
2023-09-05 12:34:37 +0200

    Added responsive, email sending and animations.

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,18 +45,100 @@ const sendEmail = (e) => {
     e.preventDefault();
 
     //serviceID - templateID - #form - publicKey
-    emailjs.sendForm('','','','');
-}
+    emailjs.sendForm('','','#contact-form','')
+    .then(() => {
+        //Show sent message
+        contactMessage.textContent = 'Message sent successfully ✅';
+
+        //Remove message after five seconds
+        setTimeout(() => {
+            contactMessage.textContent = '';
+        }, 5000);
+
+        //Clear input fields
+        contactForm.reset();
+    }, () => {
+        //Show error message
+        contactMessage.textContent = 'Message not sent (service error) ❌';
+    });
+};
 
 contactForm.addEventListener('submit', sendEmail);
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll');
+};
 
+window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
 
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link');
+        } else {
+            sectionsClass.classList.remove('active-link');
+        }
+    });
+};
+
+window.addEventListener('scroll', scrollActive);
 
 /*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button'),
+      darkTheme = 'dark-theme',
+      iconTheme = 'ri-sun-line';
+
+//Previously selected theme (if any).
+const selectedTheme = localStorage.getItem('selected-theme'),
+      selectedIcon = localStorage.getItem('selected-icon');
+
+//Get the current theme the page has.
+const getCurrentTheme = () => {
+    document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+};
+const getCurrentIcon = () => {
+    themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
+};
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 100,
+    //reset: true // Animations repeat
+});
+
+sr.reveal(`.home__perfil, .about__image, .contact__mail`, {origin:'right'});
+sr.reveal(`.home__name, .home__info,
+           .about__container .section__title-1, .about__info,
+           .contact__social, .contact__data`, {origin:'left'});
+sr.reveal(`.services__card, .projects__card`, {interval: 100});
